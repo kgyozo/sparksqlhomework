aedc0965bdf0e51d0e3487f00c1bde8fdd529907
Gyozo Karer <gyozo_karer@epam.com>
2017-05-03 11:18:38 +0200

    The solution, the tests are passed

--- src/main/scala/com/epam/training/spark/sql/Homework.scala
+++ src/main/scala/com/epam/training/spark/sql/Homework.scala
@@ -1,5 +1,8 @@
 package com.epam.training.spark.sql
 
+import java.sql.Date
+import java.time.LocalDate
+
 import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.hive.HiveContext
@@ -58,13 +61,57 @@ object Homework {
 
   }
 
-  def readCsvData(sqlContext: HiveContext, rawDataPath: String): DataFrame = ???
+  def readCsvData(sqlContext: HiveContext, rawDataPath: String): DataFrame = {
+    val df =
+      sqlContext.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .option("delimiter", ";")
+      .schema(Constants.CLIMATE_TYPE)
+      .csv(rawDataPath)
+    df.printSchema()
+    df
+  }
 
-  def findErrors(climateDataFrame: DataFrame): Array[Row] = ???
+  def findErrors(climateDataFrame: DataFrame): Array[Row] = {
+    val error = udf((cell : String) => {
+      if (cell == null || cell.isEmpty) {1} else {0}
+    });
+    climateDataFrame.select(
+       sum(error(climateDataFrame.col("observation_date"))),
+       sum(error(climateDataFrame.col("mean_temperature"))),
+       sum(error(climateDataFrame.col("max_temperature"))),
+       sum(error(climateDataFrame.col("min_temperature"))),
+       sum(error(climateDataFrame.col("precipitation_mm"))),
+       sum(error(climateDataFrame.col("precipitation_type"))),
+       sum(error(climateDataFrame.col("sunshine_hours")))
+    )
+    .collect()
+  }
 
-  def averageTemperature(climateDataFrame: DataFrame, monthNumber: Int, dayOfMonth: Int): DataFrame = ???
+  def averageTemperature(climateDataFrame: DataFrame, monthNumber: Int, dayOfMonth: Int): DataFrame = {
+   val df = climateDataFrame
+     .select(climateDataFrame.col("mean_temperature"))
+      .where(month(climateDataFrame.col("observation_date")) === monthNumber)
+      .where(dayofmonth(climateDataFrame.col("observation_date")) === dayOfMonth)
+    df.show()
+    df
+  }
 
-  def predictTemperature(climateDataFrame: DataFrame, monthNumber: Int, dayOfMonth: Int): Double = ???
+  def predictTemperature(climateDataFrame: DataFrame, monthNumber: Int, dayOfMonth: Int): Double = {
+    val filter = udf((observation_date : Date) => dateCheck(observation_date.toLocalDate, monthNumber, dayOfMonth) )
+    climateDataFrame
+      .where(filter(climateDataFrame.col("observation_date")))
+      .select(avg(climateDataFrame.col("mean_temperature")))
+      .first()
+      .getDouble(0)
+  }
+
+  def dateCheck(observation_date : LocalDate, monthNumber: Int, dayOfMonth: Int) : Boolean = {
+    (observation_date.getMonthValue == monthNumber && observation_date.getDayOfMonth == dayOfMonth) ||
+    (observation_date.minusDays(1).getMonthValue == monthNumber && observation_date.minusDays(1).getDayOfMonth == dayOfMonth) ||
+    (observation_date.plusDays(1).getMonthValue == monthNumber && observation_date.plusDays(1).getDayOfMonth == dayOfMonth)
+  }
 
 
 }
